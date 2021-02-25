719121838c0e4fad44bd3872c9be368a90834010
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2021-02-25 09:52:55 +0100

    Tous les sites développés en cours de PHP approfondi

--- Character.php
+++ Character.php
@@ -7,24 +7,163 @@ class Character
 {
 
   /**
+   * character's id
+   *
+   * @var int
+   */
+  private $_id;
+
+  /**
+   * character's name
+   *
+   * @var string
+   */
+  private $_name;
+
+  /**
    * Life represent Hp
    * When life is 0, character is dead
    *
    * @var int
    */
-  public $life = 100;
+  private $_life = 100;
+
   /**
    * Power represent character's power
    *
    * @var int
    */
-  public $power;
+  private $_power;
+
   /**
    * Mana represent character's mana
    *
    * @var int
    */
-  public $mana = 100;
+  private $_mana = 100;
+
+
+
+  /**
+   * construct function
+   *
+   * @param int $id
+   * @param string $name
+   * @return void
+   */
+  public function __construct(int $id, string $name, float $power)
+  {
+    $this->setId($id);
+    $this->setName($name);
+    $this->setPower($power);
+  }
+
+
+
+  /**
+   * get private property $_id
+   *
+   * @return int
+   */
+  public function getId() : int
+  {
+    return $this->_id;
+  }
+  /**
+   * set private property $_id
+   *
+   * @param int $id
+   * @return void
+   */
+  public function setId(int $id)
+  {
+    $this->_id = $id;
+  }
+
+
+  /**
+   * get private property $_name
+   *
+   * @return string
+   */
+  public function getName() : string
+  {
+    return $this->_name;
+  }
+  /**
+   * set private property $_name
+   *
+   * @param string $name
+   * @return void
+   */
+  public function setName(string $name)
+  {
+    $this->_name = $name;
+  }
+
+
+  /**
+   * get private property $_life
+   *
+   * @return float
+   */
+  public function getLife() : float
+  {
+    return $this->_life;
+  }
+  /**
+   * set private property $_life
+   *
+   * @param float $life
+   * @return void
+   */
+  public function setLife(float $life)
+  {
+    $this->_life = $life;
+  }
+
+
+  /**
+   * get private property $_power
+   *
+   * @return float
+   */
+  public function getPower() : float
+  {
+    return $this->_power;
+  }
+  /**
+   * set private property $_power
+   *
+   * @param float $power
+   * @return void
+   */
+  public function setPower(float $power)
+  {
+    $this->_power = $power;
+  }
+
+
+  /**
+   * get private property $_mana
+   *
+   * @return float
+   */
+  public function getMana() : float
+  {
+    return $this->_mana;
+  }
+  /**
+   * set private property $_mana
+   *
+   * @param float $mana
+   * @return void
+   */
+  public function setMana(float $mana)
+  {
+    $this->_mana = $mana;
+  }
+
 
 
   /**
@@ -36,7 +175,7 @@ class Character
   {
     if ($this->useMana(30)) {
       echo "BAAAAAAR !!!!!";
-      $this->life += 20;
+      $this->_life += 20;
     } else  {
       echo "NO MANA !! NEDD REGEN !!!";
     }
@@ -64,13 +203,13 @@ class Character
 
   public function regen(): void
   {
-    $this->mana += 50;
+    $this->_mana += 50;
   }
 
   public function useMana(int $neededmana): bool
   {
-    if ($this->mana >= $neededmana) {
-      $this->mana -= $neededmana;
+    if ($this->_mana >= $neededmana) {
+      $this->_mana -= $neededmana;
       return true;
     } else {
       return false;
@@ -79,8 +218,8 @@ class Character
 
   public function looseHp(int $lostHp): bool
   {
-    $this->life -= $lostHp;
-    if ($this->life <= 0) {
+    $this->_life -= $lostHp;
+    if ($this->_life <= 0) {
       return true;
     } else {
       return false;
--- index.php
+++ index.php
@@ -2,15 +2,42 @@
 
 require_once 'Character.php';
 
+include 'layout/header.php';
 
-$character = new Character();
+?>
 
-var_dump($character);
+<?php if (isset($_POST["character_create"])) { 
 
-$character->shout();
+  $characters = [];
 
-var_dump($character);
+  if (isset($_POST["character1_name"]) && isset($_POST["character1_power"]) && !empty($_POST["character1_name"])) {
+    $character1 = new Character(1, $_POST["character1_name"], $_POST["character1_power"]);
+    $characters[] = $character1;
+  } else {
+    echo "First character's name is empty !";
+  }
 
-$character->throwFireball();
+  if (isset($_POST["character2_name"]) && isset($_POST["character2_power"]) && !empty($_POST["character2_name"])) {
+    $character2 = new Character(2, $_POST["character2_name"], $_POST["character2_power"]);
+    $characters[] = $character2;
+  } else {
+    echo "First character's name is empty !";
+  }
 
-var_dump($character);
+  include 'layout/character.php';
+
+} ?>
+
+<form action="" method="post">
+  <label for="chararcter1_name">First character's name :</label>
+  <input name="character1_name" id="character1_name" type="text" value="" />
+  <label for="chararcter1_power">First character's power :</label>
+  <input name="character1_power" id="character2_power" type="number" value="" />
+  <label for="chararcter2_name">Second character's name :</label>
+  <input name="character2_name" id="character1_name" type="text" value="" />
+  <label for="chararcter2_power">Second character's power :</label>
+  <input name="character2_power" id="character2_power" type="number" value="" />
+  <input type="submit" name="character_create"></input>
+</form>
+
+ <?php include 'layout/footer.php'; ?>
